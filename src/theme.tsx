06c0970a00a7910a185bfede13114c62843cfc0c
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2023-07-27 05:22:43 +0800

    Add font responsiveness

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,27 +1,29 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
-const myTheme = createTheme({
-  typography: {
-    htmlFontSize: 10,
-    h1: {
-      fontSize: "6.4rem",
+const myTheme = responsiveFontSizes(
+  createTheme({
+    typography: {
+      htmlFontSize: 10,
+      h1: {
+        fontSize: "6.4rem",
+      },
+      h2: {
+        fontSize: "4.8rem",
+      },
+      h3: {
+        fontSize: "3.2rem",
+      },
+      h4: {
+        fontSize: "2.4rem",
+      },
+      h5: {
+        fontSize: "1.6rem",
+      },
+      h6: {
+        fontSize: "1.2rem",
+      },
     },
-    h2: {
-      fontSize: "4.8rem",
-    },
-    h3: {
-      fontSize: "3.2rem",
-    },
-    h4: {
-      fontSize: "2.4rem",
-    },
-    h5: {
-      fontSize: "1.6rem",
-    },
-    h6: {
-      fontSize: "1.2rem",
-    },
-  },
-});
+  })
+);
 
 export default myTheme;
