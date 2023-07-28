15f65dcf58dca19a411f57c094cb7c974ed27c0d
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2023-07-28 22:17:52 +0800

    Update about me

--- src/App.tsx
+++ src/App.tsx
@@ -185,12 +185,29 @@ function App() {
           >
             <Box>
               <Typography variant="h3">The world need more heroes.</Typography>
-              <Typography variant="h1">My name is Hafiz</Typography>
               <Typography variant="h3">
-                and I'm Spider-Man.{" "}
-                <span style={{ fontSize: "1.2rem", fontStyle: "italic" }}>
+                My name is{" "}
+                <Typography
+                  variant="h1"
+                  sx={{ display: "inline", fontWeight: 900 }}
+                >
+                  Hafiz
+                </Typography>
+              </Typography>
+              <Typography variant="h3">
+                and I'm{" "}
+                <Typography
+                  variant="h2"
+                  sx={{ display: "inline", fontWeight: 700 }}
+                >
+                  Spider-Man.{" "}
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  style={{ display: "inline", fontStyle: "italic" }}
+                >
                   (read: Web Developer)
-                </span>
+                </Typography>
               </Typography>
               <Button
                 variant="contained"
@@ -218,7 +235,7 @@ function App() {
               overflow: "hidden",
             }}
           >
-            <Typography variant="h4" sx={{ paddingTop: "3.2rem" }}>
+            <Typography variant="h2" sx={{ paddingTop: "3.2rem" }}>
               About Me
             </Typography>
             <Box
@@ -241,23 +258,41 @@ function App() {
                   display: "flex",
                   flexDirection: "column",
                   justifyContent: "space-around",
-                  margin: "0.8rem"
+                  margin: "0.8rem",
                 }}
               >
                 <Typography
-                  variant="h5"
+                  variant="body1"
                   textAlign="justify"
                   marginTop="2rem"
                   marginBottom="0.8rem"
                 >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  Coming from a health science background, I thought I would
+                  serve the community back by providing healthcare services and
+                  making a positive impact on people's lives. But God has a
+                  different plan for me. In June 2022, I took an exciting turn
+                  in my life and soon found myself embarking on a
+                  challenging-but-fun journey in programming.
+                </Typography>
+                <Typography
+                  variant="body1"
+                  textAlign="justify"
+                  marginBottom="0.8rem"
+                >
+                  Along the journey, I discovered an immense passion in web
+                  development{" "}
+                  <Typography
+                    variant="subtitle2"
+                    sx={{ display: "inline", fontStyle: "italic" }}
+                  >
+                    (and that's how I turned into Spider-Man!),{" "}
+                  </Typography>
+                  with a focus in the backend domain. With my new profound
+                  interest in programming, I am dedicated to creating innovative
+                  solutions that address real-world problems and simplify
+                  complex processes. Adhering to the best practices of software
+                  engineering is my top priority in order to build efficient,
+                  scalable, and maintainable applications.
                 </Typography>
                 <Box
                   sx={{
