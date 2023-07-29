d38055e40243b95ee05699f0f343544264ad0227
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2023-07-29 22:40:29 +0800

    Add skills section

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import {
   Button,
   Container,
   Drawer,
+  Grid,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Paper,
   Toolbar,
   Typography,
 } from "@mui/material";
@@ -31,7 +33,7 @@ import codingIllustration from "./assets/coding_illustration.png";
 import myTheme from "./theme";
 
 const drawerWidth = 240;
-const pages = ["Home", "About", "Experience", "Skills", "Projects", "Contact"];
+const pages = ["Home", "About", "Skills", "Projects", "Contact"];
 
 function App() {
   const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
@@ -117,6 +119,7 @@ function App() {
         </AppBar>
         <Box
           component="nav"
+          id="Sidebar"
           sx={{
             display: { xs: "block", sm: "none" },
             flexShrink: { sm: 0 },
@@ -161,10 +164,9 @@ function App() {
             overflow: "auto",
           }}
         >
-          <Toolbar sx={{ visibility: "hidden" }} />
+          <Toolbar id="Home" sx={{ visibility: "hidden" }} />
           <Container
             component="section"
-            id="Home"
             sx={{
               backgroundColor: "#C62828",
               color: "#FFFFFF",
@@ -179,7 +181,7 @@ function App() {
               alignItems: "center",
               alignContent: "space-evenly",
               minWidth: "100%",
-              minHeight: "100vh",
+              minHeight: { xs: "calc(100% - 56px)", sm: "calc(100% - 68px)" },
               overflow: "hidden",
             }}
           >
@@ -231,13 +233,12 @@ function App() {
               justifyContent: "center",
               alignItems: "center",
               minWidth: "100%",
-              minHeight: "100vh",
+              minHeight: { xs: "calc(100vh - 56px)", sm: "calc(100vh - 68px)" },
+              padding: "1.6rem",
               overflow: "hidden",
             }}
           >
-            <Typography variant="h2" sx={{ paddingTop: "3.2rem" }}>
-              About Me
-            </Typography>
+            <Typography variant="h2">About Me</Typography>
             <Box
               sx={{
                 display: "flex",
@@ -258,7 +259,6 @@ function App() {
                   display: "flex",
                   flexDirection: "column",
                   justifyContent: "space-around",
-                  margin: "0.8rem",
                 }}
               >
                 <Typography
@@ -345,6 +345,383 @@ function App() {
                 </Box>
               </Box>
             </Box>
+          </Container>
+          <Container
+            component="section"
+            id="Skills"
+            sx={{
+              backgroundColor: "#C62828",
+              color: "#FFFFFF",
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              alignItems: "center",
+              minWidth: "100%",
+              minHeight: { xs: "calc(100vh - 56px)", sm: "calc(100vh - 68px)" },
+              overflow: "hidden",
+              padding: "1.6rem"
+            }}
+          >
+            <Grid container spacing={2} sx={{rowGap: "1.6rem"}}>
+              <Grid item xs={12} sm={6} md={4}>
+                <Paper
+                  sx={{
+                    backgroundColor: "#FFFFFF",
+                    color: "#C62828",
+                    display: "flex",
+                    flexDirection: "column",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: 4,
+                    height: "100%",
+                    padding: "0.8rem"
+                  }}
+                >
+                  <Typography variant="h4" textAlign="center">
+                    Front-End Development
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      flexWrap: "wrap",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      padding: 2,
+                      gap: 1,
+                    }}
+                  >
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/html-1.svg"
+                      alt="HTML5"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/css-3.svg"
+                      alt="CSS3"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/logo-javascript.svg"
+                      alt="Javascript"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/typescript.svg"
+                      alt="Typescript"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/react-2.svg"
+                      alt="React"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/material-ui-1.svg"
+                      alt="Material UI"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg"
+                      alt="Bootstrap 5"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/sass-1.svg"
+                      alt="SASS"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/semantic-ui.svg"
+                      alt="Semantic UI"
+                      height={50}
+                    />
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Paper
+                  sx={{
+                    backgroundColor: "#FFFFFF",
+                    color: "#C62828",
+                    display: "flex",
+                    flexDirection: "column",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: 4,
+                    height: "100%",
+                    padding: "0.8rem"
+                  }}
+                >
+                  <Typography variant="h4" textAlign="center">
+                    Back-End Development
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      flexWrap: "wrap",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      padding: 2,
+                      gap: 1,
+                    }}
+                  >
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg"
+                      alt="NodeJS"
+                      height={50}
+                    />
+                    <img
+                      src="https://e7.pngegg.com/pngimages/925/447/png-clipart-express-js-node-js-javascript-mongodb-node-js-text-trademark-thumbnail.png"
+                      alt="ExpressJS"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/python-5.svg"
+                      alt="Python"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/django.svg"
+                      alt="Django"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/postgresql.svg"
+                      alt="PostgreSQL"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/sqlite.svg"
+                      alt="SQLite"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/redis.svg"
+                      alt="Redis"
+                      height={50}
+                    />
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Paper
+                  sx={{
+                    backgroundColor: "#FFFFFF",
+                    color: "#C62828",
+                    display: "flex",
+                    flexDirection: "column",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: 4,
+                    height: "100%",
+                    padding: "0.8rem"
+                  }}
+                >
+                  <Typography variant="h4" textAlign="center">
+                    Mobile App Development
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      flexWrap: "wrap",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      padding: 2,
+                      gap: 1,
+                    }}
+                  >
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/dart.svg"
+                      alt="Dart"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/flutter-logo.svg"
+                      alt="Flutter"
+                      height={50}
+                    />
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Paper
+                  sx={{
+                    backgroundColor: "#FFFFFF",
+                    color: "#C62828",
+                    display: "flex",
+                    flexDirection: "column",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: 4,
+                    height: "100%",
+                    padding: "0.8rem"
+                  }}
+                >
+                  <Typography variant="h4" textAlign="center">
+                    DevOps Tools
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      flexWrap: "wrap",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      padding: 2,
+                      gap: 1,
+                    }}
+                  >
+                     <img
+                      src="https://seeklogo.com/images/J/jest-logo-F9901EBBF7-seeklogo.com.png"
+                      alt="Jest"
+                      height={50}
+                    />
+                     <img
+                      src="https://cdn.worldvectorlogo.com/logos/robot-framework.svg"
+                      alt="Robot Framework"
+                      height={50}
+                    />
+                     <img
+                      src="https://seeklogo.com/images/S/selenium-logo-A1B53CEFB0-seeklogo.com.png"
+                      alt="Selenium"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/git-icon.svg"
+                      alt="Git"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/github-icon-1.svg"
+                      alt="GitHub"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/bash-2.svg"
+                      alt="Bash"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/docker-4.svg"
+                      alt="Docker"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/visual-studio-code-1.svg"
+                      alt="VS Code"
+                      height={50}
+                    />
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Paper
+                  sx={{
+                    backgroundColor: "#FFFFFF",
+                    color: "#C62828",
+                    display: "flex",
+                    flexDirection: "column",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: 4,
+                    height: "100%",
+                    padding: "0.8rem"
+                  }}
+                >
+                  <Typography variant="h4" textAlign="center">
+                    UI/UX Design
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      flexWrap: "wrap",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      padding: 2,
+                      gap: 1,
+                    }}
+                  >
+                    <img
+                      src="https://seeklogo.com/images/F/figma-logo-E4E21D3AEA-seeklogo.com.png"
+                      alt="Figma"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/adobe-illustrator-cc-icon.svg"
+                      alt="Adobe Illustrator"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/canva-1.svg"
+                      alt="Canva"
+                      height={50}
+                    />
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Paper
+                  sx={{
+                    backgroundColor: "#FFFFFF",
+                    color: "#C62828",
+                    display: "flex",
+                    flexDirection: "column",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: 4,
+                    height: "100%",
+                    padding: "0.8rem"
+                  }}
+                >
+                  <Typography variant="h4" textAlign="center">
+                    Operating Systems
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      flexWrap: "wrap",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      padding: 2,
+                      gap: 1,
+                    }}
+                  >
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/microsoft-windows-22.svg"
+                      alt="Windows"
+                      height={50}
+                    />
+                     <img
+                      src="https://cdn.worldvectorlogo.com/logos/linux-tux.svg"
+                      alt="Linux"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/ubuntu-4.svg"
+                      alt="Ubuntu"
+                      height={50}
+                    />
+                    <img
+                      src="https://cdn.worldvectorlogo.com/logos/android.svg"
+                      alt="Android"
+                      height={50}
+                    />
+                  </Box>
+                </Paper>
+              </Grid>
+            </Grid>
           </Container>
         </Box>
       </Box>
